5abcaa3ffa97347365a6842e4f744914b8eda36f
Samuel Oyetola <osoyetola@gmail.com>
2024-04-29 01:19:18 +0100

    RDS added

---
+++ awsrdsterraform.tf
@@ -0,0 +1,18 @@
+#create RDS DB with terraform
+resource "aws_db_instance" "myrds" {
+   engine              = "mysql"
+   engine_version      = "8.0.35"
+   allocated_storage   = 20
+   storage_type        = "gp2"
+   identifier          = "mydb"
+   instance_class      = "db.t3.micro"
+   username            = "admin"
+   password            = "Pa$$w0rd!ZzK4t2"
+   publicly_accessible = true
+   skip_final_snapshot = true
+
+   tags = {
+    
+     Name = "MyRDS-Demo"
+   }
+ }
